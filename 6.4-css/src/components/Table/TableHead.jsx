4b59d5555b349a88db3f97ebcd8827a819b7aa72
Christian Bohollo <7813599+hondilla@users.noreply.github.com>
2022-05-25 21:38:34 +0200

    refactors

--- 6.4-css/src/components/Table/TableHead.jsx
+++
@@ -1,8 +0,0 @@
-import TableColumns from "./TableColumns";
-
-const TableHead = ({ columns }) => 
-  <thead>
-    <TableColumns columns={ columns } />
-  </thead>;
-
-export default TableHead;
--- 7.2-composition/src/components/Table/TableRow.jsx
+++
@@ -1,7 +0,0 @@
-const TableRow = ({ row }) => 
-  <tr>
-    <td>{ row.id }</td>
-    <td>{ row.name }</td>
-  </tr>;
-
-export default TableRow;
