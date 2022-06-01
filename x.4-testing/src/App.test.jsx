612f953b9eb7bce3973d19977c55e7c51b00438f
Christian Bohollo <7813599+hondilla@users.noreply.github.com>
2022-06-01 21:30:40 +0200

    testing

---
+++ x.4-testing/src/App.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+
+it('renders App', () => {
+    render(<App />)
+
+    expect(screen.getByText('Hello Vite + React!')).toBeInTheDocument()
+})
+
+it('click count button', async () => {
+    render(<App />)
+
+    userEvent.click(screen.getByText(/count is: /i))
+
+    await waitFor(() => {
+        expect(screen.getByText('count is: 1')).toBeInTheDocument()
+    })
+})
