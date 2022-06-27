7469fd6e65cf3886a8b93c4dd0ae9ec0b69b563c
Christian Bohollo <7813599+hondilla@users.noreply.github.com>
2022-06-27 20:46:01 +0200

    abort controller

--- 8.4-rendering/src/hooks/useCountries.jsx
+++ 8.4-rendering/src/hooks/useCountries.jsx
@@ -5,9 +5,11 @@ export const useCountries = () => {
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     (async () => {
         setIsLoading(true);
-        const request = await fetch("https://restcountries.com/v3.1/all");
+        const request = await fetch("https://restcountries.com/v3.1/all", { signal: controller.signal });
         const result = await request.json();
         let countries = [];
         if(request.ok) {
@@ -18,6 +20,8 @@ export const useCountries = () => {
         setIsLoading(false);
         setCountries(countries);
     })();
+
+    return () => controller.abort();
   }, []);
 
   return [isLoading, countries];
